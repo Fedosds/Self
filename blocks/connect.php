cae954a9ba942729656527c364410c3d1af40d79
Fedosds <63261657+Fedosds@users.noreply.github.com>
2021-02-08 13:21:23 +0300

    Delete connect.php

--- blocks/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $servername = "localhost";
-  $database = "kurs";
-  $username = "root";
-  $password = "root";
-
-  $conn = mysqli_connect($servername, $username, $password, $database);
-?>
