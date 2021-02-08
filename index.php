ab17b66d257fbdbcabb1d8958554d20e8fc3d3e5
Fedosds <fedosds@gmail.com>
2021-02-08 13:53:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,48 +4,33 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/style.css">
   <title>Библиотека им Федора</title>
 </head>
 <body>
-  <?php require "blocks/header.php" ?>
-  <?php
-require_once 'blocks/connect.php';
-?>
-    <div class="container mt-5">
-      <h3 class="mb-5">Лучшие книги</h3>
-      <div class="d-flex flex-wrap">
-      <?php
-        for($i = 0; $i < 6; $i++):
-      ?>
-      <?php
-      $id = $i+1;
-      $quer = "SELECT idbook, img, description FROM book WHERE idbook = '$id'";
-      $book = mysqli_query($conn, $quer);
-      $book = mysqli_fetch_assoc($book);
-        ?>
-          <div class="card mb-5 shadow-sm">
-            <div class="card-header">
-                <img src="img/<?=$book[img]?>" alt="" style="height: 500px" class="img-thumbnail">
-                <div class="card-body">
-                  <p class="card-text" style="max-height: 100px; overflow: auto; text-overflow: ellipsis"><?=$book[description]?></p>
-                  <div class="d-flex justify-content-between align-items-stretch">
-                    <div class="btn-group">
-                      <a href="info.php?id=<?=($i+1)?>" type="button" class="btn btn-sm btn-outline-secondary">Подробнее</a>
-                      <?php
-                        if($_COOKIE['user'] == 'Fedor'):
-                      ?>
-                      <a href="sql/edit.php?id=<?=($i+1)?>" type="button" class="btn btn-sm btn-outline-secondary">Редактировать</a>
-                      <?php endif; ?>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-      <?php endfor; ?>
-      </div>
+
+  <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0">Cover</h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Contact</a>
+      </nav>
     </div>
-
-  <?php require "blocks/footer.php" ?>
-
+  </header>
+  
+  <main class="px-3">
+    <h1>Cover your page.</h1>
+    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+    <p class="lead">
+      <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+    </p>
+  </main>
+  
+  <footer class="mt-auto text-white-50">
+    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+  </footer>
 </body>
 </html>
