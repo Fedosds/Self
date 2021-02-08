b391a7bd9686f42d667e141faa8514d6a784ecdf
Fedosds <63261657+Fedosds@users.noreply.github.com>
2021-02-08 13:20:32 +0300

    Delete sql directory

--- sql/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../blocks/connect.php';
-
-$id=$_GET['id'];
-$query = "DELETE FROM book WHERE book.idbook = '$id'";
-mysqli_query($conn, $query);
-header('Location: edithion.php');
-?>
